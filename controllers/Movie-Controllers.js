ddf81b3f134c61658be154f2ea1766e6bf1b3db6
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-06-19 02:57:08 +0300

    the adding of cache

--- controllers/Movie-Controllers.js
+++ controllers/Movie-Controllers.js
@@ -2,32 +2,56 @@ const axios = require('axios');
 const Movie = require('../models/Movies-Model');
 require('dotenv').config();
 
+
+const Cache = require('../Helper/Cache');
+
+
+const cacheObj = new Cache();
+
+
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 const movieController = (req,res) => {
   let cityName = req.query.city;
   console.log(cityName);
-  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`)
-    .then(response => {
-      const arrOfMovies = [];
-      response.data.results.map(item => {
-        let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-        let movieObject = new Movie(
-          item.title,
-          item.overview,
-          item.vote_average,
-          item.vote_count,
-          imageURL,
-          item.popularity,
-          item.release_date
-        );
-        arrOfMovies.push(movieObject);
+  const requestKey = `movie-${cityName}`;
+
+  if (cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)) {
+    console.log('=====================');
+    console.log('From the cache Object');
+    console.log('=====================');
+    res.json(cacheObj[requestKey]);
+  }
+  else{
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`)
+      .then(response => {
+        const arrOfMovies = [];
+        response.data.results.map(item => {
+          let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+          let movieObject = new Movie(
+            item.title,
+            item.overview,
+            item.vote_average,
+            item.vote_count,
+            imageURL,
+            item.popularity,
+            item.release_date
+          );
+          arrOfMovies.push(movieObject);
+        });
+        console.log('=====================');
+        console.log('From the axios request');
+        console.log('=====================');
+        console.log('Storing the data from the request into our cache');
+        console.log('=====================');
+        cacheObj[requestKey] = arrOfMovies;
+        cacheObj[requestKey].timestamp = Date.now();
+        res.status(200).send(arrOfMovies);
+      })
+      .catch(error => {
+        res.status(500).send(error.message);
       });
-      res.status(200).send(arrOfMovies);
-    })
-    .catch(error => {
-      res.status(500).send(error.message);
-    });
+  }
 };
 
 module.exports = movieController;
--- controllers/Weather-Controllers.js
+++ controllers/Weather-Controllers.js
@@ -2,23 +2,48 @@ const axios = require('axios');
 const Forecast = require('../models/Weather-Model');
 require('dotenv').config();
 
+const Cache = require('../Helper/Cache');
+
+
+const cacheObj = new Cache();
+
+
+
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 
 const weatherController =  (req, res) => {
   const lat = req.query.lat;
   const lon = req.query.lon;
+  const requestKey = `weather-${lat}-${lon}`;
 
-  const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-  let newData=[];
-  axios.get(weatherBitUrl).then((data) => {
+  if (cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)) {
+    console.log('=====================');
+    console.log('From the cache Object');
+    console.log('=====================');
+    res.json(cacheObj[requestKey]);
+  } else {
+
+    const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+    let newData=[];
+    axios.get(weatherBitUrl).then((data) => {
     // console.log(data.data);
-    data.data.data.forEach((weatherData)=>{
-      newData.push(new Forecast(weatherData.datetime,weatherData.weather.description));
+      data.data.data.forEach((weatherData)=>{
+        newData.push(new Forecast(weatherData.datetime,weatherData.weather.description));
+
+        console.log('=====================');
+        console.log('From the axios request');
+        console.log('=====================');
+        console.log('Storing the data from the request into our cache');
+        console.log('=====================');
+        cacheObj[requestKey] = newData;
+        cacheObj[requestKey].timestamp = Date.now();
+        res.send(newData);
+      });
+
+    }).catch(error => {
+      res.send(error.message);
     });
-    res.send(newData);
-  }).catch(error => {
-    res.send(error.message);
-  });
+  }
 };
 
 module.exports = weatherController;
